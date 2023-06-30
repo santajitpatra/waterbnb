840fceb0b853bfef2c9444418e2050c903704eb0
Santajit Patra <business.santajitpatra@gmail.com>
2023-06-30 23:41:22 +0530

    feat : header & banner
    header & banner added

---
+++ components/Banner.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+
+function Banner() {
+  return (
+    <div className='relative h-[300px] sm:h-[400px] xl:h-[600px] 2xl:h-[700px]'>
+      <Image
+        src="https://images.pexels.com/photos/1591361/pexels-photo-1591361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="Picture of the author"
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className='absolute top-1/2 w-full text-center'>
+        <p className='text-sm sm:text-lg'>Not sure where to go ? perfect</p>
+        <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'> I'm flexible</button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner
