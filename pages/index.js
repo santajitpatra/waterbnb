2fab1b9264cb5649aeca69633f408a08071ee87d
Santajit Patra <business.santajitpatra@gmail.com>
2023-07-01 03:17:27 +0530

    feat : small card
    small card  added

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,46 @@
 import Head from "next/head";
 import Header from "@/components/Header";
 import Banner from "@/components/Banner";
+import SmallCard from "@/components/SmallCard";
 
-export default function Home() {
+
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
         <title>Waterbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
-      <Banner/>
+      <Header />
+      <Banner />
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item) => {
+              return (
+                <SmallCard
+                  key={item.img}
+                  img={item.img}
+                  location={item.location}
+                  distance={item.distance}
+                />
+              );
+            })}
+          </div>
         </section>
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://santa-api.onrender.com/citys").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
